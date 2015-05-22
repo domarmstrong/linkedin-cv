d3819325d6b3eaf26ffb8873623cf3d5b14551d2
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-05-22 14:03:54 +0100

    Add context wrapper component
    To allow passing global context to children

--- src/server/renderer.js
+++ src/server/renderer.js
@@ -5,6 +5,7 @@ import Qfs from 'q-io/fs';
 import React from 'react';
 import _ from 'underscore';
 import path from 'path';
+import util from 'util';
 
 const config = require(path.join(process.cwd(), 'config.js'));
 
@@ -33,13 +34,42 @@ function getHtmlTemplate() {
 }
 
 /**
+ * Get context extended with global context that all components can access
+ * @return (Object)
+ */
+function getContext(context) {
+  return util._extend({
+    app_name: config.app_name,
+  }, context);
+}
+
+/**
  * Render component into the html template
  * @param Component : React component
  * @param props {Object}
+ * @param context {Object}
  * @return {Promise} html string
  */
-export function render(Component, props={}) {
+export function render(Component, props={}, context={}) {
   return getHtmlTemplate().then(template => {
-    return template( React.renderToString(<Component { ...props } />) );
+    return template( React.renderToString(
+      <Container component={ Component } props={ props } context={ getContext(context) } />
+    ) );
   });
 }
+
+/**
+ * Wrapper component used for passing context to children
+ */
+class Container extends React.Component {
+  getChildContext () {
+    return this.props.context;
+  }
+  render () {
+    return <this.props.component { ...this.props.props } />
+  }
+}
+Container.childContextTypes = {
+  app_name: React.PropTypes.string.isRequired,
+  active_route: React.PropTypes.string.isRequired,
+};
