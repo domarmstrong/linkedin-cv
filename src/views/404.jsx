56d2aae6e6d0d523a1c1c9c6708b552086ebbb6e
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-05-23 19:17:02 +0100

    404 page

---
+++ src/views/404.jsx
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * Author: Dom Armstrong, Date: 21/05/15
+ */
+
+import React from 'react';
+import { Layout } from '../components/layout';
+
+export class Page404 extends React.Component {
+  render () {
+    return (
+      <Layout id="Page404">
+        <h1>404</h1>
+        <div className="sub">Sorry the requested page <em>{ this.props.url }</em> does not exist</div>
+      </Layout>
+    )
+  }
+}
