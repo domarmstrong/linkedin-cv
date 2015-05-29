bd708ee824048138e4ac52eac275882a00eb425f
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-05-29 17:14:28 +0100

    Update coverage task
    It can now be run independently and the gulp coverage task no longer
    requires to be run as `node --harmony \`which gulp\` coverage`.

--- src/gulp/tasks/coverage.js
+++ src/gulp/tasks/coverage.js
@@ -1,76 +1,28 @@
 /**
- * Note: This task requires node to be running with --harmony
- * In order to run gulp with harmony use the following:
- * ```
- * node --harmony `which gulp` coverage
- * ```
+ * Create code coverage reports
  */
 
 import gulp from 'gulp';
-import config from '../config';
-import mocha from 'gulp-mocha';
 import path from 'path';
-import { transform as babel } from 'babel';
-import istanbul from 'istanbul';
-import istanbulSourceMap from 'istanbul-coverage-source-map';
-import tap from 'gulp-tap';
+import { spawn } from 'child_process'
 
+/**
+ * Since this task requires --harmony and gulp does not run with harmony by default
+ * we can get around this by spawning a child process that runs with harmony
+ */
 gulp.task('coverage', ['clean'], function () {
-  let instrumenter = new istanbul.Instrumenter({ coverageVariable: 'coverage_id' });
-  let collector = new istanbul.Collector();
-  let reporter = new istanbul.Reporter(null, './build/coverage');
-  let conf = config.coverage;
-
-  let instrumented = {};
-  let sourceMaps = {};
-
-  istanbul.hook.hookRequire(
-    // Should the require call be transformed?
-    file => file in instrumented,
-    // Return the transformed code
-    (code, file) => instrumented[file],
-    // Options
-    {}
-  );
-
-  let ignore = conf.ignore.map(glob => '!' + glob);
-  let sources = conf.cover.concat(ignore);
-
-  return gulp.src(sources)
-    .pipe(tap(f => {
-      let relative = path.join('./', f.path.replace(path.join(process.cwd(), 'src'), ''));
-      sourceMaps[f.path] = './' + path.join(conf.mapDir, relative + '.map');
-      let transformed = babel(f.contents.toString(), {
-        sourceMaps: true,
-        sourceFileName: './' + relative,
-        sourceMapName: path.basename(relative) + '.map',
-        sourceRoot: './src',
-        //optional: ["runtime"]
-      });
-      instrumented[f.path] = instrumenter.instrumentSync(transformed.code, f.path);
-      f.path += '.map';
-      f.contents = new Buffer(JSON.stringify(transformed.map, null, 2));
-    }))
-    .pipe(gulp.dest(conf.mapDir))
-    .on('end', () => {
-      return gulp.src(sources, { read: false })
-        .pipe(tap(f => delete require.cache[path.resolve(f.path)]))
-        .pipe(tap(f => require(f.path)))
-        .on('end', () => {
-          return gulp.src(config.test.src, { read: false })
-            .pipe( mocha({ reporter: 'spec' }) )
-            .on('end', () => {
-              let coverage = JSON.parse(
-                istanbulSourceMap(
-                  global['coverage_id'],
-                  { sourceMaps: sourceMaps }
-                )
-              );
-              collector.add(coverage);
-
-              reporter.addAll(['html', 'text', 'text-summary']);
-              reporter.write(collector, true, () => console.log('done'));
-            });
-        } );
-    });
+  // Spawn a new child process running with harmony and run the coverage file
+  // Get the absolute path so the file we want to run
+  let taskPath = path.resolve(process.cwd(), 'src/server/coverage');
+  // Use babel register to parse the file and require the file executing its default function
+  let init = `
+    require('babel/register');
+    require('${ taskPath }')().then(function () {
+      // Resolve hanging process
+      process.nextTick(function () { process.exit(0) });
+    })
+  `;
+  let child = spawn('node', ['--harmony', '--harmony-proxies', '-e', init], { stdio: 'inherit' });
+  child.on('close', code => process.exit(code));
 });
+
---
+++ src/server/coverage.js
@@ -0,0 +1,145 @@
+/**
+ * Generate code coverage using istanbul/babel/mocha
+ *
+ * See gulp coverage > src/gulp/tasks/coverage
+ */
+
+import gulp from 'gulp';
+import config from '../gulp/config';
+import mocha from 'gulp-mocha';
+import path from 'path';
+import { transform as babel } from 'babel';
+import istanbul from 'istanbul';
+import istanbulSourceMap from 'istanbul-coverage-source-map';
+import tap from 'gulp-tap';
+import promisePipe from 'promisepipe';
+import Q from 'q';
+
+
+// Holds transformed and instrumented code against absolute file paths
+// eg. { '/home/foo/bar/file.js': 'file code..' }
+let instrumentedCode = {};
+// Map source files to their map files
+// eg. { '/home/foo/bar/file.js': './maps/file.js.map' }
+let sourceMaps = {};
+
+
+/**
+ * Generate coverage reports
+ * @returns {Promise}
+ */
+export default function createCoverage () {
+  // Prefix ignore globs to ignore them with gulp.src
+  let ignore = config.coverage.ignore.map(glob => '!' + glob);
+  let sources = config.coverage.cover.concat(ignore);
+
+  // Reset these module level vars
+  instrumentedCode = {};
+  sourceMaps = {};
+
+  return setUpRequireHook()
+    .then(instrumentSources(sources))
+    .then(forceRequireSources(sources))
+    .then(runAllTests)
+    .then(createCoverageReport);
+}
+
+/**
+ * Use istanbuls require hook to globally override `require` and
+ * return any code that has been transformed and instrumented
+ * @returns {Promise}
+ */
+function setUpRequireHook () {
+  istanbul.hook.hookRequire(
+    // Should the require call be transformed?
+      file => file in instrumentedCode,
+    // Return the transformed code
+    (code, file) => instrumentedCode[file],
+    // Options
+    {}
+  );
+  // Return promise to allow chain, because it reads better ;)
+  return Q();
+}
+
+function instrumentSources (sources) {
+  let instrumenter = new istanbul.Instrumenter({ coverageVariable: config.coverage.coverageVariable });
+  let { mapDir } = config.coverage;
+
+  return function () {
+    return promisePipe(
+      gulp.src(sources)
+        .pipe(tap(f => {
+          // Get the files relative path
+          let relative = path.relative(process.cwd(), f.path);
+          // Add original file path > map file path from cwd
+          sourceMaps[f.path] = path.join(mapDir, relative + '.map');
+          // Transform files with babel so istanbul can understand them
+          // and create source maps to translate coverage back later
+          let transformed = babel(f.contents.toString(), {
+            sourceMaps: true,
+            sourceFileName: relative,
+            sourceMapName: path.basename(relative) + '.map',
+            sourceRoot: './',
+          });
+          // Instrument the transformed code with istanbul
+          instrumentedCode[f.path] = instrumenter.instrumentSync(transformed.code, f.path);
+          // Modify the file object to write the source map file to the mapDir
+          f.path += '.map';
+          f.contents = new Buffer(JSON.stringify(transformed.map, null, 2));
+        }))
+        .pipe(gulp.dest(path.join(mapDir, 'src')))
+    );
+  }
+}
+
+/**
+ *
+ * @param sources source file globs for gulp.src
+ * @returns {Function,[Promise]}
+ */
+function forceRequireSources (sources) {
+  return function () {
+    return promisePipe(
+      gulp.src(sources, {read: false})
+        // Clear requires cache to make sure transformed files are returned
+        .pipe(tap(f => delete require.cache[path.resolve(f.path)]))
+        // Make sure all source files are required or they may not get code coverage
+        .pipe(tap(f => require(f.path)))
+    );
+  }
+}
+
+/**
+ * Run all tests with mocha
+ * @returns {Promise}
+ */
+function runAllTests() {
+  return promisePipe(
+    gulp.src(config.test.src, { read: false })
+      .pipe( mocha(config.test.mochaOpts) )
+  );
+}
+
+/**
+ * Collect the coverage data and create the reports
+ * @returns {Promise}
+ */
+function createCoverageReport () {
+  let collector = new istanbul.Collector();
+  let reporter = new istanbul.Reporter(null, config.coverage.reportDir);
+  let deferred = Q.defer();
+
+  // Get the coverage data from the global var that istanbul creates
+  let coverageData = global[config.coverage.coverageVariable];
+  if (! coverageData) {
+    throw new Error('Cannot find coverage data for coverageVariable: ' + config.coverage.coverageVariable);
+  }
+  // Use sourcemaps to correct the output lines for coverage report
+  let coverage = istanbulSourceMap(coverageData, { sourceMaps: sourceMaps });
+  collector.add(JSON.parse(coverage));
+
+  reporter.addAll(config.coverage.reports);
+  reporter.write(collector, true, deferred.resolve);
+  return deferred.promise;
+}
