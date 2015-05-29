8b36dcf981c814390ee0f4c4c5590350ad6dfc78
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-05-29 13:49:35 +0100

    Tidy gulp clean

--- src/gulp/tasks/clean.js
+++ src/gulp/tasks/clean.js
@@ -7,18 +7,15 @@ import clean from 'gulp-clean';
 import config from '../config';
 
 /**
- * Removed compiled files
+ * Remove generated files
  */
 gulp.task('clean', function () {
-  gulp.src(config.less.dest, { read: false }).pipe(clean());
-  gulp.src('./node_modules/install.stamp', { read: false }).pipe(clean());
+  return gulp.src(['./build', './node_modules/install.stamp'], { read: false }).pipe(clean());
 });
 
 /**
  * Hard reset the working directory including compiled files and node_modules
  **/
 gulp.task('veryclean', ['clean'], function () {
-  return gulp.src('./node_modules', { read: false })
-    .pipe(clean());
+  return gulp.src('./node_modules', { read: false }).pipe(clean());
 });
-
