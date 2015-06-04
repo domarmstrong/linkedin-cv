d0e910fafde4813968f620b9e49c7cc324408aaa
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-06-04 10:21:00 +0100

    fix error page styling

--- src/views/404.js
+++ src/views/404.js
@@ -8,11 +8,15 @@ import React from 'react';
 
 export default class Page404 extends React.Component {
   render () {
+    let router = this.context.router;
     return (
       <div id="Page404" className="error-page">
         <h1>404</h1>
-        <div className="sub">Sorry the requested page <em>{ this.props.url }</em> does not exist</div>
+        <div className="sub">Sorry the requested page <em>{ router.getCurrentPath() }</em> does not exist</div>
       </div>
     )
   }
 }
+Page404.contextTypes = {
+  router: React.PropTypes.any,
+};
