444467bf155a001e8edd5c78ae48771e52dc2103
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-06-09 17:29:16 +0100

    Use core-js to polyfill Object.assign

--- src/client/bootstrap.js
+++ src/client/bootstrap.js
@@ -4,6 +4,8 @@
  * Author: Dom Armstrong, Date: 05/06/15
  */
 
+// Import polyfills as required to keep bundle size down
+import 'core-js/fn/object/assign';
 import Client from './client';
 
 window.client = new Client();
