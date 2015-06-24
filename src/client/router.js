9f83f6adfe0874d5de037a572c78dedf7983fba0
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-06-24 10:01:46 +0100

    fix history after react-router update

--- src/client/router.js
+++ src/client/router.js
@@ -7,7 +7,7 @@
 import { Router } from 'react-router';
 import Location from 'react-router/lib/Location';
 import BrowserHistory from 'react-router/lib/BrowserHistory';
-import routes, { fetchProps } from '../app-routes';
+import routes, { fetchProps } from '../app_routes';
 import queryString from 'querystring';
 
 /** @Map */
@@ -33,12 +33,13 @@ export default {
 
     let query = queryString.parse(window.location.search.replace('?',''));
     let location = new Location(window.location.pathname, query);
+    let history = new BrowserHistory();
 
     Router.run(routes, location, [onTransition], (err, initialState, transition) => {
       if (err) throw err; // TODO error logging
 
       let router = React.render(
-        <Router { ...initialState } routes={ routes } history={ BrowserHistory } createElement={ createElement } />,
+        <Router { ...initialState } routes={ routes } history={ history } createElement={ createElement } />,
         document.querySelector('#_mount')
       );
       router.addTransitionHook(onTransition);
