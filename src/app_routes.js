b075e29ae14e6e4cfdc253bf7cbd2350c8eff0be
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-06-24 10:01:21 +0100

    renamed to use _ for consistency, added sync error catching

--- src/app_routes.js
+++ src/app_routes.js
@@ -54,17 +54,26 @@ export default (
  *   }
  * }
  *
- * @returns {Map}
+ * @returns {Promise, [Map]}
  */
 export function fetchProps (state) {
   let routeData = new Map();
 
-  return Promise.all(
-    state.branch
-      .filter(branch => branch.component && branch.component.fetchProps) // find which components have a fetchProps method
-      .map(branch => Promise.resolve(branch.component.fetchProps(state)) // fetch the data, may be promise
-        .then(data => routeData.set(branch.component, data)) // set the data against the Component
-        .catch(err => console.error('Error->fetchProps: ', err.stack)) // TODO error logging
-    )
-  ).then(() => routeData);
+  try {
+    return Promise.all(
+      state.branch
+        .filter(branch => branch.component && branch.component.fetchProps) // find which components have a fetchProps method
+        .map(branch => Promise.resolve(branch.component.fetchProps(state)) // fetch the data, may be promise
+            .then(data => routeData.set(branch.component, data)) // set the data against the Component
+            .catch(err => {
+              // TODO error logging
+              // console.log(`Error->fetchProps for: "${branch.component.name}", ${err.stack}`);
+              throw err;
+            })
+      )
+    ).then(() => routeData);
+  } catch (err) {
+    // Catch any synchronous errors
+    return Promise.reject(err);
+  }
 }
