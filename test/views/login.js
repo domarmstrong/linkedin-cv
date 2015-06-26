8340d3fbe82cf910a6a6e0f0297820909d461cc0
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-06-26 10:49:50 +0100

    login view coverage

--- test/views/login.js
+++ test/views/login.js
@@ -8,4 +8,9 @@ describe('Login page', () => {
   it('renders with no errors', () => {
     React.renderToString( <Login /> );
   });
+
+  it('renders login validation errors', () => {
+    let rendered = React.renderToStaticMarkup( <Login validation={{ login: 'login failed' }} /> );
+    assert.match(rendered, /<div class="form-validation">login failed<\/div>/);
+  });
 });
