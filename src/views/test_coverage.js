6c93c30fa7a48d3b38d29bf41252fde138c7e928
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-06-26 10:50:31 +0100

    test coverage for test coverage

--- src/views/test_coverage.js
+++ src/views/test_coverage.js
@@ -14,12 +14,27 @@
 import React from 'react';
 import autobind from 'autobind-decorator';
 import path from 'path';
+import { toAbsoluteUrl } from '../../src/request';
 
 
 export default class TestCoverage extends React.Component {
 
   componentDidMount () {
+    if (this.props.testContent) {
+      this.injectContent();
+    }
     this.hijackLinks();
+  }
+
+  /**
+   * Inject content into the iframe to allow testing
+   */
+  injectContent () {
+    let frame = React.findDOMNode(this.refs.frame);
+    let document = frame.contentDocument;
+    document.open();
+    document.write(this.props.testContent);
+    document.close();
   }
 
   /**
@@ -30,12 +45,13 @@ export default class TestCoverage extends React.Component {
   hijackLinks () {
     let frame = React.findDOMNode(this.refs.frame);
     let window = frame.contentWindow;
-    if (window.document.documentElement === null) return; // document is not loaded properly
+    if (window.document.documentElement === null) return; // document is not loaded properly (jsdom)
     let nodes = window.document.querySelectorAll('a');
     for (var i = 0; i < nodes.length; i++) {
       nodes[i].addEventListener('click', this.linkListener, false);
     }
   }
+
 
   /**
    * On click of a link stop the default action and route using react-router
@@ -50,16 +66,20 @@ export default class TestCoverage extends React.Component {
       // Only route internal urls
       if (/(http|https):\/\//.test(href)) return;
 
+      // getPath() > foo/bar/baz.html > foo/bar
       let currentWithoutFile = this.getPath().split('/').slice(0, -1).join('/');
-
+      // join link > foo/bar/qux/link.html
       let goTo = path.join(currentWithoutFile, href);
 
       // The main index is handled by the indexRoute
-      if (goTo === 'index.html') {
+      if (/^\/?index\.html/.test(goTo)) {
         goTo = '';
       }
 
-      this.context.router.transitionTo(this.getBasePath() + goTo, {}, undefined);
+      // join to basePath and remove trailing "/"
+      goTo = path.join(this.getBasePath() + goTo).replace(/\/$/, '');
+
+      this.context.router.transitionTo(goTo);
       event.preventDefault();
     }
   }
@@ -77,7 +97,6 @@ export default class TestCoverage extends React.Component {
         path += params.splat;
       }
     }
-    if (params.file) path += '/' + params.file;
     if (path.startsWith('/')) path = path.slice(1);
     return path;
   }
@@ -86,21 +105,23 @@ export default class TestCoverage extends React.Component {
    * @returns {String}
    */
   getBasePath () {
-    let routerState = this.props.routerState;
-    let path = routerState.location.pathname;
-    path = path.replace(this.getPath(), '');
+    let pathname = this.props.routerState.location.pathname;
+    let path = pathname.replace(this.getPath(), '');
     if (! path.endsWith('/')) path += '/';
     return path;
   }
 
   render () {
+    let testContent = this.props.testContent;
+    let src = testContent ? 'blank' : toAbsoluteUrl('/public/coverage/' + this.getPath());
     return (
       <div id="test-coverage">
-        <iframe ref="frame" className="test-coverage" src={ '/public/coverage/' + this.getPath() } onLoad={ this.hijackLinks } />
+        <iframe ref="frame" className="test-coverage" src={ src } onLoad={ this.hijackLinks } />
       </div>
     )
   }
 }
 TestCoverage.contextTypes = {
   router: React.PropTypes.any.isRequired,
+  testContent: React.PropTypes.string,
 };
--- test/views/test_coverage.js
+++ test/views/test_coverage.js
@@ -4,9 +4,110 @@ import { assert } from 'chai';
 import TestCoverage from '../../src/views/test_coverage';
 import React from 'react';
 import test_utils from '../test_utils';
+import sinon from 'sinon';
+
+// helper
+function clickLink (link) {
+  var event = link.ownerDocument.createEvent("HTMLEvents");
+  event.initEvent("click", true, true);
+  link.dispatchEvent(event);
+}
 
 describe('Test Coverage page', () => {
   it('renders with no errors', () => {
     return test_utils.renderWithRouter(TestCoverage);
+  });
+
+  describe('hijackLinks', () => {
+    let router;
+
+    beforeEach(() => {
+      router = { transitionTo: sinon.stub() };
+    });
+
+    it('makes local links redirect with react-router transitionTo', () => {
+      let testContent = `<body><a href="/foo">bar</a></body>`;
+      return test_utils.renderWithRouter(TestCoverage, { testContent }, { router }).then(comp => {
+        let doc = React.findDOMNode(comp.refs.frame).contentDocument;
+        let a = doc.querySelector('a');
+        clickLink(a);
+        assert.equal(router.transitionTo.args[0][0], '/foo');
+      });
+    });
+
+    it('does not hijacks links absolute urls', () => {
+      let testContent = `<body><a href="http://google.com">bar</a></body>`;
+      return test_utils.renderWithRouter(TestCoverage, { testContent }, { router }).then(comp => {
+        let doc = React.findDOMNode(comp.refs.frame).contentDocument;
+        let a = doc.querySelector('a');
+        clickLink(a);
+        assert(router.transitionTo.notCalled, 'transitionTo should not be called');
+      });
+    });
+
+    it('redirects base/path/index.html > base/path', () => {
+      let routerState = {
+        location: { pathname: 'base/path/foo.html' },
+        params: { splat: 'foo.html' }
+      };
+      let testContent = `<body><a href="/index.html">bar</a></body>`;
+      return test_utils.renderWithRouter(TestCoverage, { testContent, routerState }, { router }).then(comp => {
+        let doc = React.findDOMNode(comp.refs.frame).contentDocument;
+        let a = doc.querySelector('a');
+        clickLink(a);
+        assert.equal(router.transitionTo.args[0][0], 'base/path');
+      });
+    });
+  });
+
+  describe('getBasePath', () => {
+    it('trims the splat', () => {
+      let routerState = {
+        location: { pathname: 'foo/bar/baz/qux.html' },
+        params: { splat: 'baz/qux.html' }
+      };
+      return test_utils.renderWithRouter(TestCoverage, { routerState }).then(comp => {
+        assert.equal(comp.getBasePath(), 'foo/bar/');
+      });
+    });
+
+    it('ensures a trailing slash', () => {
+      let routerState = {
+        location: { pathname: 'foo/bar' },
+        params: { splat: null }
+      };
+      return test_utils.renderWithRouter(TestCoverage, { routerState }).then(comp => {
+        assert.equal(comp.getBasePath(), 'foo/bar/');
+      });
+    });
+  });
+
+  describe('getPath', () => {
+    it('works with an string splat', () => {
+      let routerState = {
+        params: { splat: 'foo/bar/baz.html' }
+      };
+      return test_utils.renderWithRouter(TestCoverage, { routerState }).then(comp => {
+        assert.equal(comp.getPath(), 'foo/bar/baz.html');
+      });
+    });
+
+    it('works with an array splat', () => {
+      let routerState = {
+        params: { splat: ['foo', 'bar', 'baz.html'] }
+      };
+      return test_utils.renderWithRouter(TestCoverage, { routerState }).then(comp => {
+        assert.equal(comp.getPath(), 'foo/bar/baz.html');
+      });
+    });
+
+    it('removes leading slash', () => {
+      let routerState = {
+        params: { splat: '/foo/bar/baz.html' }
+      };
+      return test_utils.renderWithRouter(TestCoverage, { routerState }).then(comp => {
+        assert.equal(comp.getPath(), 'foo/bar/baz.html');
+      });
+    });
   });
 });
