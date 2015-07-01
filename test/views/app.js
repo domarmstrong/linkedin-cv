88770fdcd8d453c28375f23279cc5f559db70713
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-07-01 13:59:05 +0100

    app tests

---
+++ test/views/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+import { assert } from 'chai';
+import App from '../../src/views/app';
+import React from 'react';
+import test_utils from '../test_utils';
+
+describe('app view', () => {
+  it('renders with no errors', () => {
+    test_utils.renderWithRouter(App, { app_name: 'test' });
+  });
+
+  describe('fetchProps', () => {
+    it('gets the app_name', () => {
+      return App.fetchProps().then(data => {
+        assert.isDefined(data.app_name);
+      });
+    });
+  });
+});
