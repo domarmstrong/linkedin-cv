2c96c998220621e2e8ecbe00ee456fb13001f59a
Dom Armstrong <dom@dom-armstrong.co.uk>
2015-07-06 16:15:52 +0100

    aync await

--- test/server/routing/publicsRoutes.js
+++ test/server/routing/publicsRoutes.js
@@ -10,67 +10,65 @@ import config from '../../../config';
 
 let baseUrl = 'http://localhost:' + config.app_port;
 
-describe('publicRoutes', () => {
+describe.only('publicRoutes', () => {
   let client;
 
-  before(() => {
+  before(async () => {
     test_utils.startServer();
-    return auth.createUser('testUser', 'testPass');
+    client = await new Client();
+    await auth.createUser('testUser', 'testPass');
   });
-  after(() => {
+  after(async () => {
     test_utils.stopServer();
-    return auth.deleteUser('testUser');
-  });
-
-  beforeEach(() => {
-    client = new Client();
+    await auth.deleteUser('testUser');
   });
 
   describe('logout', () => {
-    beforeEach(() => {
-      return client.login('testUser', 'testPass');
+    beforeEach(async () => {
+      await client.login('testUser', 'testPass');
     });
 
-    it('logs the user out', () => {
-      return request.get('/auth/is-authenticated').then(res => {
-        assert.equal(res.body, true);
-      }).then(() => {
-        return request.get('/logout');
-      }).then(() => {
-        return request.get('/auth/is-authenticated');
-      }).then(res => {
-        assert.equal(res.body, false);
-      });
+    it('logs the user out', async () => {
+      let res;
+      res = await request.get('/auth/is-authenticated');
+      assert.equal(res.body, true);
+
+      await request.get('/logout');
+
+      res = await request.get('/auth/is-authenticated');
+      assert.equal(res.body, false);
     });
 
-    it('redirects to /', () => {
-      return request.get('/logout').then(res => {
-        assert.equal(res.redirects.length, 1);
-        assert.equal(res.redirects[0], baseUrl + '/');
-      });
+    it('redirects to /', async () => {
+      let res = await request.get('/logout');
+      assert.equal(res.redirects.length, 1);
+      assert.equal(res.redirects[0], baseUrl + '/');
     });
   });
 
   describe('login', () => {
     context('request accepting html', () => {
-      it('redirects to /admin on success', () => {
-        return request.post('/auth/login')
+      it('redirects to /admin on success', async () => {
+        let res = await request.post('/auth/login')
           .accept('html')
-          .send({ username: 'testUser', password: 'testPass' })
-          .then(res => {
-            assert.equal(res.redirects.length, 1);
-            assert.equal(res.redirects[0], baseUrl + '/admin');
-          });
+          .send({ username: 'testUser', password: 'testPass' });
+
+        assert.equal(res.redirects.length, 1);
+        assert.equal(res.redirects[0], baseUrl + '/admin');
       });
 
-      it('re-renders the login page with failure on bad login', () => {
-        return request.post('/auth/login')
-          .accept('html')
-          .send({ username: 'foo', password: 'bar' }).then(res => assert.fail()).catch(err => {
-            assert.equal(err.status, 401);
-            assert.match(err.response.text, /login/);
-            assert.match(err.response.text, /Username or password incorrect/);
-          });
+      it('re-renders the login page with failure on bad login', async () => {
+        let err;
+        try {
+          await request.post('/auth/login')
+            .accept('html')
+            .send({ username: 'foo', password: 'bar' });
+        } catch (error) {
+          err = error;
+        }
+        assert.equal(err.status, 401);
+        assert.match(err.response.text, /login/);
+        assert.match(err.response.text, /Username or password incorrect/);
       });
     });
   });
